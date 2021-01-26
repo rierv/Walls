a3d7af4675cb2512ac6eff4b2ff43681918f07bc
jebbo <r.iervolino93@gmail.com>
2021-01-26 16:07:27 +0100

    small fix in the build

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -7,19 +7,15 @@ public class CharacterController : MonoBehaviour
 
     public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
     public RotationAxes axes = RotationAxes.MouseXAndY;
-    public float sensitivityX = 15F;
-    public float sensitivityY = 15F;
+    public float sensitivityX = 1F;
+    public float sensitivityY = 1F;
     public float minimumX = -360F;
     public float maximumX = 360F;
     public float minimumY = -60F;
     public float maximumY = 60F;
     float rotationX = 0F;
     float rotationY = 0F;
-    private List<float> rotArrayX = new List<float>();
-    float rotAverageX = 0F;
-    private List<float> rotArrayY = new List<float>();
-    float rotAverageY = 0F;
-    public float frameCounter = 2;
+   
     Quaternion originalRotation;
     Rigidbody rb;
     public Transform pointer;
@@ -41,52 +37,23 @@ public class CharacterController : MonoBehaviour
         }
         if (axes == RotationAxes.MouseXAndY)
         {
-            //Resets the average rotation
-            rotAverageY = 0f;
-            rotAverageX = 0f;
+            
 
             //Gets rotational input from the mouse
             rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
             rotationX += Input.GetAxis("Mouse X") * sensitivityX;
 
-            //Adds the rotation values to their relative array
-            rotArrayY.Add(rotationY);
-            rotArrayX.Add(rotationX);
-
-            //If the arrays length is bigger or equal to the value of frameCounter remove the first value in the array
-            if (rotArrayY.Count >= frameCounter)
-            {
-                rotArrayY.RemoveAt(0);
-            }
-            if (rotArrayX.Count >= frameCounter)
-            {
-                rotArrayX.RemoveAt(0);
-            }
-
-            //Adding up all the rotational input values from each array
-            for (int j = 0; j < rotArrayY.Count; j++)
-            {
-                rotAverageY += rotArrayY[j];
-            }
-            for (int i = 0; i < rotArrayX.Count; i++)
-            {
-                rotAverageX += rotArrayX[i];
-            }
-
-            //Standard maths to find the average
-            rotAverageY /= rotArrayY.Count;
-            rotAverageX /= rotArrayX.Count;
 
             //Clamp the rotation average to be within a specific value range
-            rotAverageY = ClampAngle(rotAverageY, minimumY, maximumY);
-            rotAverageX = ClampAngle(rotAverageX, minimumX, maximumX);
+            rotationY = ClampAngle(rotationY, minimumY, maximumY);
+            rotationX = ClampAngle(rotationX, minimumX, maximumX);
 
             //Get the rotation you will be at next as a Quaternion
-            Quaternion yQuaternion = Quaternion.AngleAxis(rotAverageY, Vector3.left);
-            Quaternion xQuaternion = Quaternion.AngleAxis(rotAverageX, Vector3.up);
+            Quaternion yQuaternion = Quaternion.AngleAxis(rotationY, Vector3.left);
+            Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
 
             //Rotate
-            camera.transform.localRotation = Quaternion.Lerp(camera.transform.localRotation, originalRotation * xQuaternion * yQuaternion, 1);
+            camera.transform.localRotation = Quaternion.Lerp(camera.transform.localRotation, originalRotation * xQuaternion * yQuaternion, .5f);
             pointer.localRotation = originalRotation * xQuaternion;
         }
     }
